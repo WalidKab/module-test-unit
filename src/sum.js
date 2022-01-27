d868e4f6302ff6d06e8c5f1f19653fa49919320e
Walid <walid.kabli@le-campus-numerique.fr>
2022-01-27 09:42:01 +0100

    Exo 1 - 4

--- src/sum.js
+++ src/sum.js
@@ -1,3 +1,8 @@
 // Implement the sum function here
 export function sum(a, b) {
+    if(typeof a !== "number" || typeof b !== "number"){
+        return "You must provide 2 numbers!"
+    }
+
+    return a + b;
 }
--- test/sum.test.js
+++ test/sum.test.js
@@ -2,5 +2,10 @@
 import {sum} from "../src/sum";
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('is number', () => {
+    expect(sum("a", "hgrfue")).toBe("You must provide 2 numbers!")
 });
